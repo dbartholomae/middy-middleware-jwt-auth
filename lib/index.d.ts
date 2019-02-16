0c81448f607067769e429c0f866b2bd451a87f06
Daniel Bartholomae <daniel@bartholomae.name>
2019-02-16 18:06:27 +0100

    chore: remove liv folder

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/** @ignore */
-/** An additional comment to make sure Typedoc attributes the comment above to the file itself */
-import middleware from './JWTAuthMiddleware';
-export default middleware;
-export * from './JWTAuthMiddleware';
