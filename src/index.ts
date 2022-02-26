89eebf1081c143ffeb9308a097bb17f22db1fcb1
Daniel Bartholomae <daniel@bartholomae.name>
2022-02-26 10:41:46 +0100

    chore: update typedoc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/** @ignore */
-/** An additional comment to make sure Typedoc attributes the comment above to the file itself */
 /* istanbul ignore file */
 import middleware from './JWTAuthMiddleware'
 export default middleware
