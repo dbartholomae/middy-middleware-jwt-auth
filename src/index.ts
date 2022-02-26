2fa72a17a0d162e0f1712570927b565f8f4a7fcb
Daniel Bartholomae <daniel@bartholomae.name>
2022-02-26 10:32:27 +0100

    chore: update jest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 /** @ignore */
 /** An additional comment to make sure Typedoc attributes the comment above to the file itself */
-/* istanbul ignore next */
+/* istanbul ignore file */
 import middleware from './JWTAuthMiddleware'
-/* istanbul ignore next */
 export default middleware
-/* istanbul ignore next */
 export * from './JWTAuthMiddleware'
