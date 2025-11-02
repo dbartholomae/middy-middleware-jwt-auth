9c2b4ed0061f020ca34a314e9df859f3187fe9be
Daniel Bartholomae <daniel@bartholomae.name>
2025-11-02 20:38:04 +0100

    Replace commitlint with PR title lint

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "body-max-line-length": [0, "always", Infinity],
-    "scope-case": [0, "always", []],
-    "subject-case": [0, "always", []],
-  },
-};
